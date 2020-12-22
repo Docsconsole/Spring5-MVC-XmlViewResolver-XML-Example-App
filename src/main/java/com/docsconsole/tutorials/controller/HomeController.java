cfae56b86967fa61fb4952f2e5dd4cc5e233a4b3
AmarSivas <amarsivas.b@gmail.com>
2020-12-22 16:08:45 +0530

    change package names
    change package names

--- src/main/java/com/docsconsole/tutorials/controller/HomeController.java
+++ src/main/java/com/docsconsole/tutorials/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.docsconsole.tutorials.springmvc5.controller;
+package com.docsconsole.tutorials.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
